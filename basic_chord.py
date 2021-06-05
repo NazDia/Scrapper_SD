ec35a9a5944460cebb59c9755a559979985c027e
AdrianHP <adrianmatcom@gmail.com>
2021-06-05 13:18:18 -0400

    chord implementation

---
+++ basic_chord.py
@@ -0,0 +1,119 @@
+
+import random
+import hashlib
+k = 6
+MAX = 2**k
+
+def getHash(key):
+    result = hashlib.sha1(key.encode())
+    return int(result.hexdigest(), 16) % MAX
+
+
+def decr(value,size):
+    if size <= value:
+        return value - size
+    else:
+        return MAX-(size-value)
+
+def between(value,init,end):
+    if init == end:
+        return True
+    elif init > end :
+        shift = MAX - init
+        init = 0
+        end = (end +shift)%MAX
+        value = (value + shift)%MAX
+    return init < value < end
+
+def Ebetween(value,init,end):
+    if value == init:
+        return True
+    else:
+        return between(value,init,end)
+
+def betweenE(value,init,end):
+    if value == end:
+        return True
+    else:
+        return between(value,init,end)
+
+class Node:
+    def __init__(self,id):
+        self.id = id
+        self.finger = {}
+        self.start = {}
+        for i in range(k):
+            self.start[i] = (self.id+(2**i)) % (2**k)
+
+    def successor(self):
+        return self.finger[0]
+    
+    def find_successor(self,id):  
+        if betweenE(id,self.predecessor.id,self.id):
+            return self
+        n = self.find_predecessor(id)
+        return n.successor()
+    
+    def find_predecessor(self,id):
+        if id == self.id:
+            return self.predecessor
+        n1 = self
+        while not betweenE(id,n1.id,n1.successor().id):
+            n1 = n1.closest_preceding_finger(id)
+        return n1
+    
+    def closest_preceding_finger(self,id):
+        for i in range(k-1,-1,-1):
+            if between(self.finger[i].id,self.id,id):
+                return self.finger[i]
+        return self
+        
+    
+    def join(self,n1):
+        if self == n1:
+            for i in range(k):
+                self.finger[i] = self
+            self.predecessor = self
+        else:
+            self.init_finger_table(n1)
+            self.update_others()  
+          
+            
+    def init_finger_table(self,n1):
+        self.finger[0] = n1.find_successor(self.start[0])
+        self.predecessor = self.successor().predecessor
+        self.successor().predecessor = self
+        self.predecessor.finger[0] = self
+        for i in range(k-1):
+            if Ebetween(self.start[i+1],self.id,self.finger[i].id):
+                self.finger[i+1] = self.finger[i]
+            else :
+                self.finger[i+1] = n1.find_successor(self.start[i+1])
+
+    def update_others(self):
+        for i in range(k):
+            prev  = decr(self.id,2**i)
+            p = self.find_predecessor(prev)
+            if prev == p.successor().id:
+                p = p.successor()
+            p.update_finger_table(self,i)
+            
+    def update_finger_table(self,s,i):
+        if Ebetween(s.id,self.id,self.finger[i].id) and self.id!=s.id:
+                self.finger[i] = s
+                p = self.predecessor
+                p.update_finger_table(s,i)
+
+    def update_others_leave(self):
+        for i in range(k):
+            prev  = decr(self.id,2**i)
+            p = self.find_predecessor(prev)
+            p.update_finger_table(self.successor(),i)
+
+    def leave(self):
+        self.successor().predecessor = self.predecessor
+        self.predecessor.setSuccessor(self.successor())
+        self.update_others_leave()
+        
+    def setSuccessor(self,succ):
+        self.finger[0] = succ
